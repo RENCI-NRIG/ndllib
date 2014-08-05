d533b10c1a6aa5662f4ecbfeeeddd65db4908bfe
pruth <pruth@9b6b0240-9c15-4950-99ad-f55ef948cf6a>
2014-08-05 15:43:30 +0000

    added testDriver and its build requirnments

---
+++ orca-external/ndllib/trunk/src/main/java/orca/ndllib/TestDriver.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package orca.ndllib;
+
+/**
+ * @author geni-orca
+ *
+ */
+public class TestDriver {
+	public static void main(String [] args){
+
+    	System.out.println("ndllib TestDriver");
+	}
+}
